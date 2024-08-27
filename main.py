c520d6d651f316ec140d4285d2ad1ca04ec7c186
Marius Schäffer <marius.schaeffer@web.de>
2024-08-27 12:50:23 +0200

    feat: add analyse functions

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
+import json
+import re
 import sys
 import os
 
-PATTERNS = 
+PATTERNS = None
+
+def analyse_pattern(line: str, pattern: str):
+    return re.search(pattern, line)
+
+def find_context(content, idx) -> tuple:
+    i = idx - 1
+    while i >= 0:
+        if re.search(r"^\s*class\s+\w+\s*:", content[i]) or re.search(r"^\s*def\s+\w+\s*\(.*\)\s*:", content[i]):
+            return content[i].strip(), i
+        i -= 1
+    return "root", -1
 
 def analyse_folder(path: str):
-    pass
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            analyse_file(os.path.join(root, file))
+        for dir in dirs:
+            analyse_folder(os.path.join(root, dir))
 
 def analyse_file(path: str):
-    pass
+    print(f"Analyzing file: {path}")
+    with open(path, "r") as f:
+        content = f.readlines()
+    for idx, line in enumerate(content):
+        for patternName in PATTERNS:
+            regex_pattern = PATTERNS[patternName]["match"]
+            if analyse_pattern(line, regex_pattern):
+                context, context_line_nr = find_context(content, idx)
+                print(f"Pattern {patternName} found in line: {idx+1} with context: {context} at line: {context_line_nr+1}")
+
+def load_patterns(path: str):
+    with open(path, "r") as f:
+        return json.load(f)
 
 def main():
     pattern_path = sys.argv[1]
@@ -16,6 +45,8 @@ def main():
         raise FileNotFoundError(pattern_path)
     if not os.path.exists(path):
         raise FileNotFoundError(path)
+    global PATTERNS
+    PATTERNS = load_patterns(pattern_path)
     if os.path.isdir(path):
         analyse_folder(path)
     else:
